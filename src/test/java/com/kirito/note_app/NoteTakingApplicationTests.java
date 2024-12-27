ea30feac809b4363ec85fa132eaffe2cd80dea10
whsad <kirito20212021@163.com>
2024-12-27 09:27:19 +0800

    first commit

---
+++ src/test/java/com/kirito/note_app/NoteTakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirito.note_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
